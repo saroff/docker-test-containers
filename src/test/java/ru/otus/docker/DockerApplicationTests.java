24412b205dcbb33ea458832c33a249774ee43e61
saroff <tarasoff154@gmail.com>
2019-09-20 19:49:46 +0300

    Updated example

--- src/test/java/ru/otus/docker/DockerApplicationTests.java
+++ src/test/java/ru/otus/docker/DockerApplicationTests.java
@@ -1,6 +1,8 @@
 package ru.otus.docker;
 
 import com.mongodb.MongoClient;
+import org.bson.Document;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -11,6 +13,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.testcontainers.containers.GenericContainer;
 
 import java.util.HashMap;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -29,5 +32,8 @@ public class DockerApplicationTests {
         HashMap<String, Object> map = new HashMap<>();
         map.put("name", "Steve");
         mongoTemplate.save(map, "someCollection");
+        final List<Document> result = mongoTemplate.findAll(Document.class, "someCollection");
+        Assert.assertEquals("Result size is 1", 1, result.size());
+        Assert.assertEquals("Name is the same", result.get(0).get("name"), "Steve");
     }
 }
